70021ce8df8dec27b6acd127dde91a9358106c8f
root <root@DESKTOP-HBNA8R2.localdomain>
2023-06-22 15:50:31 -0700

    working ros melodic with test_drive.py

---
+++ build/basic_rover/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/catkin_ws/src/basic_rover
+# Build directory: /root/catkin_ws/build/basic_rover
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
